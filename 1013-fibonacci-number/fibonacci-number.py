72f6ed9b30637744575dd81ae27c1cd4c5248a70
SAEED NM <150129271+saeedhilu@users.noreply.github.com>
2024-12-18 09:36:37 +0530

    Time: 15 ms (66.34%) | Memory: 12.5 MB (6.31%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,15 +1,23 @@
 class Solution(object):
     def fib(self, n):
         """
-        :type n: int
-        :rtype: int
-        """
-        fab = [0,1]
-        if n == 2:
-            return 1
-        for i in range(n):
-            fab_new_value = fab[i] + fab[i+1]
-            # print(fab_new_value)
-            fab.append(fab_new_value)
+        # :type n: int
+        # :rtype: int
+        # """
+        # fab = [0,1]
+        # if n == 2:
+        #     return 1
+        # for i in range(n):
+        #     fab_new_value = fab[i] + fab[i+1]
+        #     # print(fab_new_value)
+        #     fab.append(fab_new_value)
         
-        return fab[n]       
+        # return fab[n]       
+        pre, curr = 0,1
+        if n == 0 :
+            return 0
+        if n == 1:
+            return 1
+        for _ in range(2,n+1):
+            pre, curr = curr, pre + curr
+        return curr
