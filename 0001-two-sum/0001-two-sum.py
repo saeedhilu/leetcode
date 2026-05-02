dec4cabadecaa0ab6847598e88a1b9378c263c03
SAEED NM <150129271+saeedhilu@users.noreply.github.com>
2026-05-02 19:53:35 +0530

    Time: 0 ms (100%), Space: 20.4 MB (43.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,11 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for  index , item in enumerate(nums):
-            print("item is :", item , index)
             value = target - item
             if  value in seen:
                 return [seen[value], index]
-
             seen[item] = index
                
 
